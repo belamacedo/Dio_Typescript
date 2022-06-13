2ccf7b7b8a690349ce3ecfd5adafdfd2aa672abe
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-06-13 16:04:58 -0300

    sobre unknown

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 "use strict";
-function printaValoresNumericos(numero1, numero2) {
-    console.log(numero1 + numero2);
+let anyEstaDeVolta;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = "teste";
+anyEstaDeVolta = 5;
+let strinTest = "verificar";
+strinTest = anyEstaDeVolta;
+let unknownValor;
+unknownValor = 3;
+unknownValor = "opa";
+unknownValor = true;
+unknownValor = "vai sim";
+let stringTest2 = "agora vai";
+stringTest2 = unknownValor;
+if (typeof unknownValor === "string") {
+    stringTest2 = unknownValor;
 }
-function somarValoresNumericosETratar(numero1, numero2, callback) {
-    let resultado = numero1 + numero2;
-    return callback(resultado);
-}
-function aoQuadrado(numero) {
-    return numero * numero;
-}
-function dividirPorEleMesmo(numero) {
-    return numero / numero;
-}
-console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
-console.log(somarValoresNumericosETratar(1, 3, dividirPorEleMesmo));
--- app.ts
+++ app.ts
@@ -1,23 +1,20 @@
-function printaValoresNumericos(numero1: number, numero2: number): void {
-	console.log(numero1 + numero2);
-}
+let anyEstaDeVolta: any;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = "teste";
+anyEstaDeVolta = 5;
 
-function somarValoresNumericosETratar(
-	numero1: number,
-	numero2: number,
-	callback: (numero: number) => number
-): number {
-	let resultado = numero1 + numero2;
-	return callback(resultado);
-}
+let strinTest: string = "verificar";
+strinTest = anyEstaDeVolta;
 
-function aoQuadrado(numero: number): number {
-	return numero * numero;
-}
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = "opa";
+unknownValor = true;
+unknownValor = "vai sim";
 
-function dividirPorEleMesmo(numero: number): number {
-	return numero / numero;
-}
+let stringTest2: string = "agora vai";
+stringTest2 = unknownValor;
 
-console.log(somarValoresNumericosETratar(1, 3, aoQuadrado));
-console.log(somarValoresNumericosETratar(1, 3, dividirPorEleMesmo));
+if (typeof unknownValor === "string") {
+	stringTest2 = unknownValor;
+}
